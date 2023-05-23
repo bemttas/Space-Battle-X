13395a40afc28aa1337f7875f7b031d75072bb93
Eduardo-M-Martins <martins-dudu@hotmail.com>
2023-05-23 11:18:08 -0300

    menu e globals

---
+++ Scripts/globals.gd
@@ -0,0 +1,81 @@
+extends Node
+
+# Gravity variable
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# Respawn variables
+var respawn_point
+
+# Game variables
+var lv1_enable = true
+var lv2_enable = false
+var lv3_enable = false
+var lv1_stars = 0
+var lv2_stars = 0
+var lv3_stars = 0
+var lv1_col1 = false
+var lv1_col2 = false
+var lv1_col3 = false
+var lv2_col1 = false
+var lv2_col2 = false
+var lv2_col3 = false
+var lv3_col1 = false
+var lv3_col2 = false
+var lv3_col3 = false
+var nickname = ""
+
+# Save variables
+var save_path = "user://.sbx64.dat"
+
+# Load data
+func loadsave():
+	var file = File.new()
+	if file.open_encrypted_with_pass(save_path, File.READ, "!@#$%¨*()_+123654789?     ...     By Bemttas&Martins") == OK:
+		var data = file.get_line()
+		file.close()
+		var json = JSON.parse(data)
+		lv1_enable = json["lv1_enable"]
+		lv2_enable = json["lv2_enable"]
+		lv3_enable = json["lv3_enable"]
+		lv1_stars = json["lv1_stars"]
+		lv2_stars = json["lv2_stars"]
+		lv3_stars = json["lv3_stars"]
+		lv1_col1 = json["lv1_col1"]
+		lv1_col2 = json["lv1_col2"]
+		lv1_col3 = json["lv1_col3"]
+		lv2_col1 = json["lv2_col1"]
+		lv2_col2 = json["lv2_col2"]
+		lv2_col3 = json["lv2_col3"]
+		lv3_col1 = json["lv3_col1"]
+		lv3_col2 = json["lv3_col2"]
+		lv3_col3 = json["lv3_col3"]
+		nickname = json["nickname"]
+
+# Create new data
+func createsave():
+	var directory = Directory.new()
+	if directory.dir_exists(save_path):
+		File.remove(save_path)
+	var data = {
+		"lv1_enable": lv1_enable,
+		"lv2_enable": lv2_enable,
+		"lv3_enable": lv3_enable,
+		"lv1_stars": lv1_stars,
+		"lv2_stars": lv2_stars,
+		"lv3_stars": lv3_stars,
+		"lv1_col1": lv1_col1,
+		"lv1_col2": lv1_col2,
+		"lv1_col3": lv1_col3,
+		"lv2_col1": lv2_col1,
+		"lv2_col2": lv2_col2,
+		"lv2_col3": lv2_col3,
+		"lv3_col1": lv3_col1,
+		"lv3_col2": lv3_col2,
+		"lv3_col3": lv3_col3,
+		"nickname": nickname
+	}
+	var json = JSON.print(data)
+	var file = File.new()
+	if file.open(save_path, File.WRITE) == OK:
+		file.store_string(json)
+		file.close()
--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,16 +1,73 @@
 extends Control
 
+# Save variables
+var exist_save = false
+
+# Init function
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	var directory = Directory.new()
+	if directory.dir_exists(Globals.save_path):
+		exist_save = true
+		$VBoxContainer/Button.set("custom_colors/font_color_focus", Color(1,1,0,1))
+		$VBoxContainer/Button.set("custom_colors/font_color_hover", Color(1,1,0,1))
+		$VBoxContainer/Button.set("custom_colors/font_color", Color(1,1,1,1))
+		$VBoxContainer/Button.grab_focus() 
+	else:
+		$VBoxContainer/Button.set("custom_colors/font_color_focus", Color(0.29,0.29,0,1))
+		$VBoxContainer/Button.set("custom_colors/font_color_hover", Color(0.29,0.29,0,1))
+		$VBoxContainer/Button.set("custom_colors/font_color", Color(0.29,0.29,0.29,1))
+		$VBoxContainer/Button2.grab_focus()
+
+func _on_Button_mouse_entered():
+	$VBoxContainer/Button2.release_focus()
+	$VBoxContainer/Button2.set_focus_mode(0)
+	$VBoxContainer/Button3.release_focus()
+	$VBoxContainer/Button3.set_focus_mode(0)
+	$VBoxContainer/Button.grab_focus()
+func _on_Button_mouse_exited():
+	$VBoxContainer/Button2.set_focus_mode(2)
+	$VBoxContainer/Button3.set_focus_mode(2)
 func _on_Button_pressed():
-	$transition.get_node("ColorRect").get_node("animation").play("in")
-	yield(get_tree().create_timer(1.0), "timeout")
-	
-	get_tree().change_scene(("res://Scenes/World.tscn"))
-	
-	pass # Replace with function body.
+	if exist_save:
+		Globals.loadsave()
+		$transition.get_node("ColorRect").get_node("animation").play("in")
+		yield(get_tree().create_timer(1.0), "timeout")
+		#mudar pra menu de fases
+		get_tree().change_scene(("res://Scenes/World.tscn"))
 
+func _on_Button2_mouse_entered():
+	$VBoxContainer/Button.release_focus()
+	$VBoxContainer/Button.set_focus_mode(0)
+	$VBoxContainer/Button3.release_focus()
+	$VBoxContainer/Button3.set_focus_mode(0)
+	$VBoxContainer/Button2.grab_focus()
+func _on_Button2_mouse_exited():
+	$VBoxContainer/Button.set_focus_mode(2)
+	$VBoxContainer/Button3.set_focus_mode(2)
+func _on_Button2_pressed():
+	if exist_save:
+		pass #menu de overwrite
+	else:
+		Globals.loadsave()
+		$transition.get_node("ColorRect").get_node("animation").play("in")
+		yield(get_tree().create_timer(1.0), "timeout")
+		get_tree().change_scene(("res://Scenes/World.tscn"))
 
+func _on_Button3_mouse_entered():
+	$VBoxContainer/Button2.release_focus()
+	$VBoxContainer/Button2.set_focus_mode(0)
+	$VBoxContainer/Button.release_focus()
+	$VBoxContainer/Button.set_focus_mode(0)
+	$VBoxContainer/Button3.grab_focus()
+func _on_Button3_mouse_exited():
+	$VBoxContainer/Button2.set_focus_mode(2)
+	$VBoxContainer/Button.set_focus_mode(2)
 func _on_Button3_pressed():
 	$transition.get_node("ColorRect").get_node("animation").play("in")
 	yield(get_tree().create_timer(1.0), "timeout")
 	get_tree().quit()
-	pass # Replace with function body.
+
+func _process(delta):
+	$ParallaxBackground.scroll_offset.x -= 100 * delta
+	$ParallaxBackground.scroll_offset.y -= 100 * delta * 0.5
