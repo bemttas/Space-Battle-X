cc87cfd2cd3e73fa58c34312964842b3db0b5a33
Eduardo-M-Martins <martins-dudu@hotmail.com>
2023-06-30 13:29:43 -0300

    final

--- Scripts/Pause.gd
+++ Scripts/Pause.gd
@@ -10,6 +10,10 @@ func _process(_delta):
 			if get_tree().paused == false:
 				get_tree().paused = true
 				$ColorRect.visible = true
+				#$ColorRect2.visible = true
+				#$VBoxContainer/TextEdit.visible = true
 			else:
 				get_tree().paused = false
 				$ColorRect.visible = false
+				#$ColorRect2.visible = false
+				#$VBoxContainer/TextEdit.visible = false
